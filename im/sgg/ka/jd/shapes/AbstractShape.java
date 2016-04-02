7c40878401f89943f77228c7c0f7cda1515ef687
Sergiy Gorichenko <m@sgg.im>
2016-04-02 15:48:42 +0300

    les 03 / 03 / 02 AbstractShape

--- im/sgg/ka/jd/shapes/AbstractShape.java
+++ im/sgg/ka/jd/shapes/AbstractShape.java
@@ -2,11 +2,6 @@ package im.sgg.ka.jd.shapes;
 
 import java.awt.*;
 
-/**
- * Created by sergiy on 23.03.16.
- * Java Developer lessons
- * kademika.com
- */
 public abstract class AbstractShape {
     protected int x,y,size,sizeX,sizeY,stroke;
     protected Color color;
--- im/sgg/ka/jd/shapes/Square.java
+++ im/sgg/ka/jd/shapes/Square.java
@@ -2,11 +2,6 @@ package im.sgg.ka.jd.shapes;
 
 import java.awt.*;
 
-/**
- * Created by sergiy on 23.03.16.
- * Java Developer lessons
- * kademika.com
- */
 public class Square extends Rectangle {
 
     public Square() {
